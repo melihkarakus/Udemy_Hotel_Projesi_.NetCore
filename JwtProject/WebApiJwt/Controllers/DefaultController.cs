39e30f2047735aa48cde94b1eab80dded5135f9b
Melih karakuş <melih.karakus1818@gmail.com>
2023-11-18 14:41:28 +0300

    76-84 JWT

---
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApiJwt.Models;
+
+namespace WebApiJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult TokunOlustur()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+        [HttpGet("[action]")]
+        public IActionResult AdminTokunOlustur()
+        {
+            return Ok(new CreateToken().CreateTokenAdmin());
+        }
+
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Test2()
+        {
+            return Ok("Hoşgeldiniz");
+        }
+
+        [Authorize(Roles = "Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Test3()
+        {
+            return Ok("Token başarılı bir şekilde giriş yaptı.");
+        }
+
+    }
+}
---
+++ JwtProject/WebApiJwt/Models/CreateToken.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace WebApiJwt.Models
+{
+    public class CreateToken
+    {
+        public string TokenCreate() 
+        {
+            // Veriyi UTF-8 formatında baytlara çevir
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi");
+
+            // Baytları kullanarak bir simetrik güvenlik anahtarı oluştur
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+
+            // Güvenlik anahtarını kullanarak bir imza oluşturucu oluştur
+            SigningCredentials signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            // JWT (Json Web Token) oluştur
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken
+                (
+                    issuer: "http://localhost",            // Token'ın hangi tarafından oluşturulduğunu belirtir
+                    audience: "http://localhost",          // Token'ın hangi tarafına yönlendirildiğini belirtir
+                    notBefore: DateTime.Now,                // Token'ın hangi tarihten önce kullanılamayacağını belirtir
+                    expires: DateTime.Now.AddMinutes(3),   // Token'ın ne zaman geçerliliğini yitireceğini belirtir
+                    signingCredentials: signingCredentials  // Token'ı imzalamak için kullanılan imza bilgilerini belirtir
+                );
+
+            // JWT token'ını ele almak için bir JWT token yöneticisi oluştur
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+
+            // JWT token'ını yazıya çevir
+            return jwtSecurityTokenHandler.WriteToken(jwtSecurityToken);
+        }
+
+
+        public string CreateTokenAdmin()
+        {
+            // Metni UTF-8 formatında baytlara çevir
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi");
+
+            // Baytları kullanarak bir simetrik güvenlik anahtarı oluştur
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+
+            // Güvenlik anahtarını ve imza algoritmasını kullanarak imza bilgileri oluştur
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            // JWT içinde taşınacak iddia (claim) bilgilerini tanımla
+            List<Claim> claims = new List<Claim>()
+{
+    new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()), // Kullanıcı kimliği
+    new Claim(ClaimTypes.Role, "Admin"),                               // Kullanıcının rolü (Admin)
+    new Claim(ClaimTypes.Role, "Visitor")                              // Kullanıcının rolü (Visitor)
+};
+
+            // JWT oluştur
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken
+                (
+                issuer: "http://localhost",                // Token'ın hangi tarafından oluşturulduğunu belirtir
+                audience: "http://localhost",              // Token'ın hangi tarafına yönlendirildiğini belirtir
+                notBefore: DateTime.Now,                    // Token'ın hangi tarihten önce kullanılamayacağını belirtir
+                expires: DateTime.Now.AddSeconds(3),        // Token'ın ne zaman geçerliliğini yitireceğini belirtir
+                signingCredentials: credentials,            // Token'ı imzalamak için kullanılan imza bilgilerini belirtir
+                claims: claims                             // Token içinde taşınacak iddia (claim) bilgilerini belirtir
+                );
+
+            // JWT token'ını ele almak için bir JWT token yöneticisi oluştur
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+
+            // JWT token'ını yazıya çevir ve döndür
+            return jwtSecurityTokenHandler.WriteToken(jwtSecurityToken);
+        }
+    }
+}
